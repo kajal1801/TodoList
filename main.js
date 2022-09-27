1ac6d3c56beb10e0ffd5763b53efaa8b3a0d15d6
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-27 21:41:11 +0530

    Added localStorage

--- main.js
+++ main.js
@@ -1,74 +1,56 @@
-window.addEventListener('load', () =>{
-    todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const form = document.querySelector('#new-task-form');
-    const input = document.querySelector('#new-task-input');
-    const list_el = document.querySelector('#tasks');
-    
-    
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault();
+showTasks();
 
-        const task = input.value;
-        if(!task){
-            alert("Please enter a task");
-            return;
-        }
-
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el);
-
-
-        const task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
-
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_actions_el);
-        list_el.appendChild(task_el);
-
-        input.value = "";
-
-        task_edit_el.addEventListener('click', () => {
-            if(task_edit_el.innerText.toLowerCase() == "edit"){
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = "Save";
-            }
-            else{
-                task_input_el.setAttribute("readonly", "readonly");
-                task_edit_el.innerText = "Edit";
-            }
-        });
-
-        task_delete_el.addEventListener('click', () => {
-            list_el.removeChild(task_el);
-        });
-
-        todos.push(task);
-        localStorage.setItem('todos', JSON.stringify(todos));
-
-        e.target.removeEventListener();
-    });
+let new_task_submit = document.getElementById("new-task-submit");
+new_task_submit.addEventListener("click", function (e) {
+  let new_task_input = document.getElementById("new-task-input");
+  let tasks = localStorage.getItem("tasks");
+  if (tasks == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(tasks);
+  }
+  taskObj.push(new_task_input.value);
+  localStorage.setItem("tasks", JSON.stringify(taskObj));
+  new_task_input.value = "";
+  console.log(taskObj);
+  showTasks();
 });
+
+function showTasks() {
+  let tasks = localStorage.getItem("tasks");
+  if (tasks == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(tasks);
+  }
+  let html = "";
+  taskObj.forEach(function (element, index) {
+    html += `
+        <div class="content">
+            <p class="task">${element}</p>
+        </div>
+        <div class="actions">
+            <button id="${index}" onclick="deleteNode(${index})" class="delete" >Delete</button>
+        </div>
+        `;
+  });
+  let taskEle = document.getElementById("tasks");
+  if (taskObj != 0) {
+    taskEle.innerHTML = html;
+  } else {
+    taskEle.innerHTML = `Nothing to show! Use "Add a Task" section above to add tasks.`;
+  }
+}
+
+function deleteNode(index) {
+  let tasks = localStorage.getItem("tasks");
+  if (tasks == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(tasks);
+  }
+
+  taskObj.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(taskObj));
+  showTasks();
+}
